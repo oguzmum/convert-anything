42bff95cf3beceb59780de1e575839fec9a0ba1f
Oguz Mumcu <oguzmum@gmail.com>
2026-01-13 23:49:13 +0100

    first basic ui elements

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="app/templates")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("index.html", {"request": request})
